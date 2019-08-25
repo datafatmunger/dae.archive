42d784bfb73c718ed6a651432293a2909819212f
JBG <jbg@peerparty.org>
2019-08-25 13:30:53 +0200

    solr schema location

--- bin/reset_solr.sh
+++ bin/reset_solr.sh
@@ -2,6 +2,6 @@
 
 su -c "/opt/solr/bin/solr delete -p 8983 -c dae " solr
 su -c "/opt/solr/bin/solr create -p 8983 -c dae -d /opt/solr/example/files/conf/" solr
-curl -X POST -H 'Content-type:application/json' --data-binary @/root/dae.json http://localhost:8983/solr/dae/schema
+curl -X POST -H 'Content-type:application/json' --data-binary @/archive/jbg/solr/dae.json http://localhost:8983/solr/dae/schema
 
 service solr restart
