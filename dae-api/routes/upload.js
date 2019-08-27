4507c290f4b4a4f10e80f198a706e384cf6d09bd
JBG <jbg@peerparty.org>
2019-08-27 18:29:06 +0200

    add_file with spaces

--- dae-api/routes/upload.js
+++ dae-api/routes/upload.js
@@ -6,8 +6,8 @@ app.post('/upload', auth.authorization, function(req, res, next) {
   try {
     for(const [key, file] of Object.entries(req.files)) {
       console.log(file)
-      const cmd = '/usr/local/bin/add_file.sh "' +
-        file.tempFilePath + ' ' +
+      const cmd = '/usr/local/bin/add_file.sh ' +
+        file.tempFilePath + ' "' +
         file.name + '" ' +
         req.user.name
       console.log('CMD: ', cmd)
