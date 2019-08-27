0cd93df7d2c7e781d9825b1d7d2a0f784d899502
JBG <jbg@peerparty.org>
2019-08-27 18:10:13 +0200

    add_file with spaces

--- bin/add_file.sh
+++ bin/add_file.sh
@@ -3,10 +3,10 @@
 echo $1 $2 $3
 
 sudo -u $3 -H sh -c "git clone /home/$3/archive.git /tmp/archive.$3.tmp"
-sudo -u $3 -H sh -c "cp $1 /tmp/archive.$3.tmp/$2"
+sudo -u $3 -H sh -c "cp \"$1\" \"/tmp/archive.$3.tmp/$2\""
 
 pushd /tmp/archive.$3.tmp
-sudo -u $3 -H sh -c "git add $2"
+sudo -u $3 -H sh -c "git add \"$2\""
 sudo -u $3 -H sh -c "git commit -am \"API added file\""
 sudo -u $3 -H sh -c "git push origin master"
 popd
