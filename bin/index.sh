c37af5fcee8b300b260121b0c970e1c586836100
JBG <jbg@peerparty.org>
2019-09-15 08:25:01 +0200

    fixes for andre's search issues

--- bin/index.sh
+++ bin/index.sh
@@ -3,13 +3,18 @@
 #bash $PWD/reset_solr.sh 
 
 find /archive -type f -print0 | while IFS= read -r -d $'\0' line; do
+  #echo "$line"
   USER=$(echo "$line" | awk '{split($0,a,"/"); print a[3]}')
   FILE=$(echo "$line" | xargs -I{} basename {})
   DIR=$(echo "$line"  | xargs -I{} dirname {})
+  EXT="${FILE##*.}"
+  BASE="${FILE%.*}"
 
   #echo $USER
   #echo $DIR
   #echo $FILE
+  #echo $EXT
+  #echo $BASE
 
   DATE=$(ls -l --time-style="+%Y-%m-%dT%H:%M:%SZ" "$line" | awk '{split($0,a," "); print a[6]}')
   #echo $DATE
@@ -25,11 +30,17 @@ find /archive -type f -print0 | while IFS= read -r -d $'\0' line; do
 
   if [ $EXT == 'txt' ] || [ $EXT == 'md' ]; then
     CONTENTS=$(cat $line | sed 's/\"/\\\"/g')
-  else
-    CONTENTS=''
+    #echo $CONTENTS
   fi
 
-  JSON="[{\"id\": \"$ID\", \"date\": \"$DATE\", \"name\": \"$FILE\", \"path\": \"$DIR\", \"user\": \"$USER\", \"contents\": \"$CONTENTS\"}]"
+  CONTENTS_JSON=""
+  if [ -z "$CONTENTS" ]; then
+    CONTENTS_JSON=", \"contents\": \"$CONTENTS\""
+  fi
+
+  JSON="[{\"id\": \"$ID\", \"date\": \"$DATE\", \"name\": \"$FILE\", \"base\": \"$BASE\", \"ext\": \"$EXT\", \"path\": \"$DIR\", \"user\": \"$USER\" $CONTENTS_JSON}]"
+
+  echo $JSON
 
   curl -d "$JSON" http://localhost:8983/solr/dae/update
 
