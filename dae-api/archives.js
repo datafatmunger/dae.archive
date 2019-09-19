659cd78ff17aa8c5091c7fae76b9ab676227a7e9
JBG <jbg@hacker.coffee>
2019-09-19 13:09:32 +0200

    fix fork error

--- dae-api/archives.js
+++ dae-api/archives.js
@@ -4,8 +4,11 @@ const exec = require('child_process').exec
 exports.fork = (archive, user) => {
   return new Promise((resolve, reject) => {
     exec('/usr/local/bin/fork.sh ' +
-      archive + ' ',
-      user.name)
+      archive + ' ' +
+      username, (error, stdout, stderr) => {
+        if(error) reject(error)
+        resolve(true)
+      })
   })
 }
 
--- dae-api/routes/fork.js
+++ dae-api/routes/fork.js
@@ -3,9 +3,9 @@ const auth = require('../authorization'),
 
 app.post('/fork', auth.authorization, async (req, res, next) => {
   try {
-    res.send(
+    res.send({ success:
       await archives.fork(req.user, req.body.archive)
-    )
+    })
   } catch(err) {
     next(err)
   }
