a7880a92080846d392c91578fb34ca23c234df19
JBG <jbg@peerparty.org>
2019-10-16 20:23:08 +0200

    just me

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -16,6 +16,7 @@ echo "Done."
 # Set a new Solr schema, incase there was an update. - JBG
 echo "Setting new Solr schema..."
 /usr/local/bin/reset_solr.sh
+sleep 10s
 echo "Done."
 
 # Run the index
