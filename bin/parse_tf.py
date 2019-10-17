2fe92076a33b3b457c49291e4fe4216217c8fd44
jbg <jbg@blackbox.dae>
2019-10-17 10:48:04 +0200

    add python scripts

---
+++ bin/parse_tf.py
@@ -0,0 +1,11 @@
+import fileinput
+import re
+import json
+
+tags = []
+for line in fileinput.input():
+    line = re.sub(r'\(.*\)', '', line)
+    tags = tags + line.split(',')
+    tags = list(map(lambda s: s.strip(), tags))
+
+print(json.dumps(tags))
