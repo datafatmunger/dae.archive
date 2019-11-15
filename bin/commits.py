be0f507db39d1de9b1a53673a53ad2f15bc36aab
jbg <jbg@example.com>
2019-11-15 11:20:49 +0100

    add commits

---
+++ bin/commits.py
@@ -0,0 +1,29 @@
+import git
+import sys
+
+repo = git.Repo(sys.argv[1])
+#commits = list(repo.iter_commits("master", max_count=5)))
+commits = list(repo.iter_commits("master"))
+
+def is_exists(filename, sha):
+    """Check if a file in current commit exist."""
+    files = repo.git.show("--pretty=", "--name-only", sha)
+    if filename in files:
+        return True
+
+def get_file_commits(filename):
+    file_commits = []
+    for commit in commits:
+        if is_exists(filename, commit.hexsha):
+            file_commits.append(commit)
+
+    return file_commits
+
+file_commits = get_file_commits(sys.argv[2])
+
+#print(file_commits)
+a = []
+for commit in commits:
+    a.append(commit.message.strip())
+
+print(a)
