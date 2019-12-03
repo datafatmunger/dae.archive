c03ef4ca0e531b9d0ee0c928e099bb4fcdc63f81
jbg <jbg@example.com>
2019-12-03 11:47:40 +0100

    remove post recieve during fork

--- bin/fork.sh
+++ bin/fork.sh
@@ -3,5 +3,6 @@
 echo $1 $2
 
 cp -r /home/$1/archive.git /home/$2/$1.archive.git
+rm /home/$2/$1.archive.git/hooks/post-receive
 chown -R $2 /home/$2/$1.archive.git
 chgrp -R $2 /home/$2/$1.archive.git
