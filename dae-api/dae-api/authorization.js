aa6b14b5ee37efd7f20fac0145838c0a226dcbfe
jbg <jbg@example.com>
2020-03-19 10:48:55 +0000

    mongo based auth

--- dae-api/dae-api/authorization.js
+++
@@ -1,9 +0,0 @@
-const users = require('./users')
-
-exports.authorization = async (req, res, next) => {
-  if(!req.user) {
-    res.status(401).send(JSON.stringify({error: "Unauthorized"}))
-  }
-  else next()
-}
-
