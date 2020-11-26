db1b13ec310e36c397ed3ec295f754f17f4eae2a
André Fincato <work@andrefincato.info>
2020-11-26 16:25:36 +0100

    rename file

--- bin/dae-wiki-install.sh
+++ bin/dae-wiki-install.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "running install.sh"
 
-NAME=$(grep NAME .env | xargs)
-NAME=${NAME#*=}
-
-PW=$(grep PW .env | xargs)
-PW=${PW#*=}
-
 git config -f .gitmodules submodule.dae-wiki.branch docker
 
 git submodule init
@@ -17,6 +11,8 @@ mkdir -p dae-wiki/public/assets
 # else docker fails?
 rm dae-wiki/public/package-lock.json && rm dae-wiki/app/package-lock.json
 
+# this copy of dae-wiki's content folder is on andré's vps
+# under ssh key verification
 if [ ! -d ./vols/wiki ]; then
     cd ./vols &&\
     git clone dae-wiki@andrefincato.info:content.git wiki &&\
