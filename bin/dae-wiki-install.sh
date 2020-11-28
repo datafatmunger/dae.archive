4239b25fe9bc59c0b3ef2368a9607d6177b2746a
André Fincato <work@andrefincato.info>
2020-11-28 17:57:20 +0100

    clean up dae-wiki-install.sh instructions

--- bin/dae-wiki-install.sh
+++ bin/dae-wiki-install.sh
@@ -8,21 +8,7 @@ git submodule update --recursive --remote
 
 mkdir -p dae-wiki/public/assets
 
-# else docker fails?
 rm dae-wiki/public/package-lock.json && rm dae-wiki/app/package-lock.json
-
-# this copy of dae-wiki's content folder is on andré's vps
-# under ssh key verification
-if [ ! -d ./vols/wiki ]; then
-    cd ./vols &&\
-    git clone dae-wiki@andrefincato.info:content.git wiki &&\
-        git checkout master &&\
-    git config pull.rebase false
-else
-    cd ./vols/wiki && git checkout master && git pull --all
-fi
-
-
 
 echo "all done"
 
