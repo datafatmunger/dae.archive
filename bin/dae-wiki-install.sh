ae472a49fce64d9524d0d354bd88533892f5ad88
André Fincato <work@andrefincato.info>
2020-11-30 10:51:38 +0100

    this is part of a general setup command now

--- bin/dae-wiki-install.sh
+++ bin/dae-wiki-install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo "running install.sh"
 
-git config -f .gitmodules submodule.dae-wiki.branch master
-
-git submodule init
-git submodule update --recursive --remote
-
 mkdir -p dae-wiki/public/assets
 
 rm dae-wiki/public/package-lock.json && rm dae-wiki/app/package-lock.json
